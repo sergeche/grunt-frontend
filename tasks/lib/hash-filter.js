eedc5cc24ff10b7f9bef802245abaeada4653440
Anton Ignatov <anton.ignatov@inn.ru>
2014-08-07 18:37:33 +0400

    Code refactoring

--- tasks/lib/hash-filter.js
+++ tasks/lib/hash-filter.js
@@ -1,20 +1,11 @@
-var _ = require('underscore');
-
-var mapping = [
-		'ad',
-		'ga',
-		'pop'
-	],
-	replaceList = '1234567890';
+var replaceList = '1234567890';
 
 module.exports = function(str) {
 	if (!str) {
 		return str;
 	}
 
-	_.each(mapping, function(value) {
-		str = str.replace(value, replaceList.slice(0, value.length));
+	return ('' + str).replace(/ad|ga|pop/gi, function(match) {
+		return replaceList.slice(0, match.length);
 	});
-
-	return str;
 }
